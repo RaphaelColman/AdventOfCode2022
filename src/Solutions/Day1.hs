6a4629279c9b34ef3837ede87390db3ea399410c
raphael <rafi.colman@googlemail.com>
2022-12-02 12:11:45 +0000

    Day 1 part 1 and 2

--- src/Solutions/Day1.hs
+++ src/Solutions/Day1.hs
@@ -1,30 +1,39 @@
 module Solutions.Day1
-  ( aoc1
-  ) where
+    ( aoc1
+    ) where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions, printTestSolutions)
-import           Data.List           (tails)
-import           Text.Trifecta       (Parser, TokenParsing (token), integer,
-                                      some)
-import Common.ListUtils (window3, window2)
+import           Common.AoCSolutions     (AoCSolution (MkAoCSolution),
+                                          printSolutions, printTestSolutions)
+import           Common.ListUtils        (window2, window3)
+import           Control.Applicative     ((<|>))
+import           Control.Monad.Extra     (skip)
+import           Data.Functor            (($>))
+import           Data.List               (sort, tails)
+import           Text.Parser.Combinators (Parsing (eof), skipOptional)
+import           Text.Trifecta           (CharParsing (anyChar), Parser,
+                                          TokenParsing (token), integer,
+                                          integer', newline, some, whiteSpace)
 
 aoc1 :: IO ()
 aoc1 = do
   printSolutions 1 $ MkAoCSolution parseInput part1
   printSolutions 1 $ MkAoCSolution parseInput part2
 
-type Depths = [Integer]
+type Elf = [Integer]
+type Elves = [Elf]
 
-parseInput :: Parser Depths
+parseInput :: Parser Elves
 parseInput = do
-  some $ token integer
+  some $ parseElf <* (skipOptional newline <|> skipOptional eof)
+  where parseElf :: Parser [Integer]
+        parseElf = some $ integer' <* newline
 
-part1 :: Depths -> Int
-part1 = sonarSweep
+part1 :: Elves -> Integer
+part1 = maximum . map sum
 
-part2 :: Depths -> Int
-part2 = sonarSweep . map (\(x, y, z) -> x + y + z) . window3
-
-sonarSweep :: Depths -> Int
-sonarSweep = length . filter id . map (\(x, y) -> y > x) . window2
+part2 :: Elves -> Integer
+part2 = sum . 
+        take 3 .
+        reverse .
+        sort .
+        map sum
