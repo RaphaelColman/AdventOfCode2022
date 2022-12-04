31de9ea2f0cb9fcc435e1f06356d754da3ee20f2
raphael <rafi.colman@googlemail.com>
2022-12-04 14:45:03 +0000

    Type safe assertions for day 3

--- src/Solutions/Day3.hs
+++ src/Solutions/Day3.hs
@@ -2,14 +2,17 @@ module Solutions.Day3
     ( aoc3
     ) where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions, printTestSolutions)
-import           Data.Char           (isUpper, ord)
-import           Data.List           (intersect, nub)
-import           Data.List.Split     (chunksOf)
-import qualified Data.Set            as S
-import           Text.Trifecta       (CharParsing (anyChar, string), Parser,
-                                      TokenParsing (token), letter, some)
+import           Common.AoCSolutions  (AoCSolution (MkAoCSolution),
+                                       printSolutions, printTestSolutions)
+import           Common.FunctorUtils  (fmap2)
+import           Control.Monad        (guard, unless)
+import           Control.Monad.Except (MonadError (throwError))
+import           Data.Char            (isUpper, ord)
+import           Data.List            (intersect, nub)
+import           Data.List.Split      (chunksOf)
+import qualified Data.Set             as S
+import           Text.Trifecta        (CharParsing (anyChar, string), Parser,
+                                       TokenParsing (token), letter, some)
 
 aoc3 :: IO ()
 aoc3 = do
@@ -21,18 +24,21 @@ type Rucksack = String
 parseInput :: Parser [Rucksack]
 parseInput = some $ token $ some letter
 
-part1 :: [Rucksack] -> Integer
-part1 = sum . map (charValue . commonLetter)
+part1 :: [Rucksack] -> Either String Integer
+part1 rucksacks = do
+  values <- traverse (fmap charValue . commonLetter) rucksacks
+  pure $ sum values
 
-part2 :: [Rucksack] -> Integer
-part2 = sum . map charValue . badges
+part2 :: [Rucksack] -> Either String Integer
+part2 rucksacks = fmap sum $ fmap2 charValue $ badges rucksacks
 
-commonLetter :: Rucksack -> Char
-commonLetter rucksack = if length commonLetters == 1
-                        then head commonLetters
-                        else error "More than one common letter"
-  where (p1, p2) = splitAt (length rucksack `div` 2) rucksack
-        commonLetters = nub $ p1 `intersect` p2
+commonLetter :: Rucksack -> Either String Char
+commonLetter rucksack = do
+  unless (even (length rucksack)) $ throwError ("Number of items is not even: " ++ rucksack)
+  let (p1, p2) = splitAt (length rucksack `div` 2) rucksack
+  let commonLetters = nub $ p1 `intersect` p2
+  unless (length commonLetters == 1) $ throwError ("More than one common letter found: " ++ rucksack)
+  pure $ head commonLetters
 
 --ASCII A = 65 (so always subtract 38 to make it 27)
 --ASCII a = 97 (so always subtract 96 to make it 1)
@@ -41,7 +47,10 @@ charValue c
   | isUpper c = toInteger $ ord c - 38
   | otherwise = toInteger $ ord c - 96
 
-badges :: [Rucksack] -> String
-badges rucksacks = map head intersections
-  where groups = chunksOf 3 rucksacks
-        intersections = map (\[s1, s2, s3] -> s1 `intersect` s2 `intersect` s3) groups
+badges :: [Rucksack] -> Either String [Char]
+badges rucksacks = do
+  let groups = chunksOf 3 rucksacks
+  unless (all ((==3) . length) groups) $ throwError "Total number of rucksacks is not divisible by 3"
+  let intersections = map (nub . \[s1, s2, s3] -> s1 `intersect` s2 `intersect` s3) groups
+  unless (all ((==1) . length) intersections) $ throwError ("More than one common item in groups: " ++ show intersections)
+  pure $ map head intersections
