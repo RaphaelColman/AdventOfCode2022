63b3732d8968106089833e862b831d8c69dc0261
raphael <rafi.colman@googlemail.com>
2022-12-06 08:18:48 +0000

    Day 6

--- src/Solutions/Day6.hs
+++ src/Solutions/Day6.hs
@@ -1,10 +1,15 @@
 module Solutions.Day6
-  ( aoc6
-  ) where
+    ( aoc6
+    ) where
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+                                      printSolutions, printTestSolutions)
+import           Common.ListUtils    (windowN)
+import           Data.Foldable       (find)
+import           Data.List           (nub)
+import           GHC.OldList         (findIndex)
+import           Text.Trifecta       (CharParsing (anyChar), Parser, manyTill,
+                                      newline, some)
 
 aoc6 :: IO ()
 aoc6 = do
@@ -12,10 +17,15 @@ aoc6 = do
   printSolutions 6 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser String
-parseInput = undefined
+parseInput = manyTill anyChar newline
 
-part1 :: String -> String
-part1 = undefined
+part1 :: [Char] -> Maybe Int
+part1 = findMarker 4
 
-part2 :: String -> String
-part2 = undefined
+part2 :: [Char] -> Maybe Int
+part2 = findMarker 14
+
+findMarker :: Eq a => Int -> [a] -> Maybe Int
+findMarker amount input = (+amount) <$> findIndex allDifferent windows
+  where windows = windowN amount input
+        allDifferent xs = length (nub xs) == length xs
