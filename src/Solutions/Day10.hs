85f46e18b596256f4d42b7e183a18444aa350f80
raphael <rafi.colman@googlemail.com>
2022-12-26 19:58:17 +0000

    Day 10 part 2

--- src/Solutions/Day10.hs
+++ src/Solutions/Day10.hs
@@ -1,20 +1,25 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-module Solutions.Day10
-    ( aoc10
-    ) where
+module Solutions.Day10 where
 
 import           Common.AoCSolutions     (AoCSolution (MkAoCSolution),
                                           printSolutions, printTestSolutions)
+import           Common.Geometry         (Point, renderVectorMap)
 import           Control.Applicative     (some)
-import           Data.Foldable           (Foldable (foldl'))
+import           Data.Foldable           (Foldable (foldl', toList))
+import qualified Data.Foldable           as Seq
 import           Data.Functor            (($>))
+import qualified Data.Map                as M
 import           Data.Sequence           (ViewR ((:>)), (|>))
 import qualified Data.Sequence           as Seq
 import           GHC.Base                (Alternative ((<|>)))
+import           Linear                  (V2 (V2))
+import           Linear.V2               (R1 (_x), V2 (V2))
 import           Text.Parser.Char        (CharParsing (string))
 import           Text.Parser.Combinators (try)
 import           Text.Parser.Token       (TokenParsing (token), integer)
 import           Text.Trifecta           (Parser, integer)
+import Control.Lens.Getter ((^.))
+import Common.Debugging (traceLns)
 
 data Instruction
   = Addx !Integer
@@ -25,7 +30,7 @@ type CpuState = Seq.Seq Integer
 
 aoc10 :: IO ()
 aoc10 = do
-  --printSolutions 10 $ MkAoCSolution parseInput part1
+  printSolutions 10 $ MkAoCSolution parseInput part1
   printSolutions 10 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser [Instruction]
@@ -38,15 +43,17 @@ parseInstruction = do
   where parseAddx = Addx <$> (string "addx " *> integer)
         parseNoop = string "noop" $> Noop
 
+part1 :: [Instruction] -> Maybe Integer
 part1 = extractSignalStrength . applyInstructions
 
-part2 = id
+part2 :: [Instruction] -> String
+part2 instructions = traceLns (renderVectorMap grid) ""
+  where grid = drawPixels instructions
 
 extractSignalStrength :: Seq.Seq Integer -> Maybe Integer
 extractSignalStrength state = do
   let cycleNumbers :: [Integer] = [20, 60, 100, 140, 180, 220]
   registers <- traverse ((`Seq.lookup` state) . fromInteger . (\x -> x - 1)) cycleNumbers
-  --pure $ zip cycleNumbers registers
   pure $ sum $ zipWith (*) cycleNumbers registers
 
 applyInstructions :: [Instruction] -> CpuState
@@ -56,3 +63,17 @@ applyInstructions = foldl' doInstruction $ Seq.singleton 1
           Addx n -> let newReg = register + n in state |> register |> newReg
           Noop   -> state |> register
           where _ :> register = Seq.viewr state
+
+pixels :: [Point]
+pixels = [V2 x y | y <- [0..height-1], x <- [0..width-1]]
+  where (width, height) = (40, 6)
+
+
+drawPixels :: [Instruction] -> M.Map Point Char
+drawPixels instructions = grid
+  where finishedState :: [Int] = map fromInteger $ toList $ applyInstructions instructions
+        grid = M.fromList $ zipWith combine pixels finishedState
+        combine :: Point -> Int -> (Point, Char)
+        combine pt register = if (pt ^. _x) `elem` [register-1, register, register+1]
+                              then (pt, '#')
+                              else (pt, '.')
