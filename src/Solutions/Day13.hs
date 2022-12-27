027dee78be5c74511da9b5c178576bfa19cc76ab
raphael <rafi.colman@googlemail.com>
2022-12-27 16:05:57 +0000

    Day 13 part 1 and 2

--- src/Solutions/Day13.hs
+++ src/Solutions/Day13.hs
@@ -1,21 +1,60 @@
 module Solutions.Day13
-  ( aoc13
-  ) where
+    ( aoc13
+    ) where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import           Common.AoCSolutions     (AoCSolution (MkAoCSolution),
+                                          printSolutions, printTestSolutions)
+import           Control.Applicative     (Alternative ((<|>)))
+import           Data.List               (elemIndex, sort)
+import           Text.Parser.Combinators (Parsing (try), count)
+import           Text.Trifecta           (Parser, brackets, commaSep, integer,
+                                          parens, some)
+import Common.FunctorUtils (fmap2)
+
+data Value
+  = VInt !Integer
+  | VList ![Value]
+  deriving (Eq, Show)
+
+instance Ord Value where
+  compare (VInt left) (VInt right)      = compare left right
+  compare left@(VList _) right@(VInt _) = compare left (VList [right])
+  compare left@(VInt _) right@(VList _) = compare (VList [left]) right
+  compare (VList left) (VList right)    = compare left right
+
+type PacketPair = (Value, Value)
 
 aoc13 :: IO ()
 aoc13 = do
-  printSolutions 13 $ MkAoCSolution parseInput part1
-  printSolutions 13 $ MkAoCSolution parseInput part2
+  printSolutions 13 $ MkAoCSolution parsePacketPairs part1
+  printSolutions 13 $ MkAoCSolution parseAllPackets part2
 
-parseInput :: Parser String
-parseInput = undefined
+parsePacketPairs :: Parser [PacketPair]
+parsePacketPairs = some parsePair
+  where parsePair = do
+        [p1, p2] <- count 2 parseVList
+        pure (p1, p2)
 
-part1 :: String -> String
-part1 = undefined
+parseAllPackets :: Parser [Value]
+parseAllPackets = some parseVList
 
-part2 :: String -> String
-part2 = undefined
+parseVList :: Parser Value
+parseVList = do
+  values <- brackets (commaSep go)
+  pure $ VList values
+  where go = try parseVList <|> try parseVInt
+        parseVInt = VInt <$> integer
+
+part1 :: [PacketPair] -> Integer
+part1 = sum . map fst . filter ((==LT) . snd) . zip [1..] . map (uncurry compare)
+
+part2 :: [Value] -> Maybe Int
+part2 packets = product <$> fmap2 (+1) indices
+  where ordered = orderPackets packets
+        indices = traverse (`elemIndex` ordered) [extra2, extra6]
+
+orderPackets :: [Value] -> [Value]
+orderPackets packets = sort $ extra2:extra6:packets
+
+extra2 = VList [VList [VInt 2]]
+extra6 = VList [VList [VInt 6]]
