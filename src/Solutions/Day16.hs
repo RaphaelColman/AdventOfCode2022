4a532c2d9e9ded7a425662136244ecf1ef3121e9
raphael <rafi.colman@googlemail.com>
2023-01-01 15:00:35 +0000

    Day 16: automate finding a good estimate for the graph

--- src/Solutions/Day16.hs
+++ src/Solutions/Day16.hs
@@ -2,45 +2,47 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 module Solutions.Day16
     ( aoc16
     ) where
 
-import           Common.AoCSolutions       (AoCSolution (MkAoCSolution),
-                                            printSolutions, printTestSolutions)
-import           Common.FunctorUtils       (fmap2)
-import           Common.ListUtils          (freqs)
-import           Control.Applicative       (many, some, (<|>))
-import           Control.Monad.Cont        (MonadTrans (lift))
-import           Control.Monad.Reader      (MonadReader (ask), Reader,
-                                            ReaderT (ReaderT, runReaderT),
-                                            runReader)
-import           Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
-import           Data.Foldable             (Foldable (toList), foldrM,
-                                            maximumBy)
-import           Data.Function             (on, (&))
-import           Data.Functor              (($>))
-import           Data.Graph.AStar          (aStar, aStarM)
-import qualified Data.HashMap.Strict       as HM
-import           Data.HashSet              (HashSet)
-import qualified Data.HashSet              as HS
-import           Data.Hashable             (Hashable)
-import           Data.Ord                  (comparing)
-import           Debug.Trace               (traceShow)
-import           GHC.Generics              (Generic)
-import           GHC.OldList               (sort, tails)
-import           Text.Parser.Combinators   (Parsing (try))
-import           Text.Printf               (printf)
-import           Text.Trifecta             (CharParsing (anyChar, char, string),
-                                            Parser, TokenParsing (token),
-                                            commaSep, integer, letter, space,
-                                            whiteSpace)
-import Common.HashSetUtils (hsFilterM)
-
-aoc16 :: IO ()
-aoc16 = do
-  printSolutions 16 $ MkAoCSolution parseInput part1
-  --printSolutions 16 $ MkAoCSolution parseInput part2
+import           Common.AoCSolutions        (AoCSolution (MkAoCSolution),
+                                             printSolutions, printTestSolutions)
+import           Common.FunctorUtils        (fmap2)
+import           Common.HashSetUtils        (hsFilterM)
+import           Common.ListUtils           (freqs)
+import           Control.Applicative        (many, some, (<|>))
+import           Control.Lens               (makeLenses)
+import           Control.Lens.Getter        ((^.))
+import           Control.Monad.Cont         (MonadTrans (lift))
+import           Control.Monad.Reader       (MonadReader (ask), Reader,
+                                             ReaderT (ReaderT, runReaderT),
+                                             runReader)
+import           Control.Monad.Reader.Class (asks)
+import           Control.Monad.Trans.Maybe  (MaybeT (MaybeT, runMaybeT))
+import           Data.Foldable              (Foldable (toList), find, foldrM,
+                                             maximumBy)
+import           Data.Function              (on, (&))
+import           Data.Functor               (($>))
+import           Data.Graph.AStar           (aStar, aStarM)
+import qualified Data.HashMap.Strict        as HM
+import           Data.HashSet               (HashSet)
+import qualified Data.HashSet               as HS
+import           Data.Hashable              (Hashable)
+import           Data.Ord                   (comparing)
+import           Debug.Trace                (traceShow, trace)
+import           GHC.Generics               (Generic)
+import           GHC.OldList                (sort, tails)
+import           Text.Parser.Combinators    (Parsing (try))
+import           Text.Printf                (printf)
+import           Text.Trifecta              (CharParsing (anyChar, char, string),
+                                             Parser, TokenParsing (token),
+                                             commaSep, integer, letter, space,
+                                             whiteSpace)
+import Data.Maybe (fromMaybe)
+import Data.HashMap.Internal.Strict (alter)
+import Safe (maximumByMay)
 
 data ValveData
   = MkValveData
@@ -66,6 +68,14 @@ data ValveState
 
 instance Hashable ValveState
 
+makeLenses ''ValveData
+makeLenses ''ValveState
+
+aoc16 :: IO ()
+aoc16 = do
+  printSolutions 16 $ MkAoCSolution parseInput part1
+  --printSolutions 16 $ MkAoCSolution parseInput part2
+
 parseInput :: Parser (HM.HashMap String ValveData)
 parseInput = do
   lines <- some $ token parseValveLine
@@ -85,8 +95,7 @@ part1 mp = doSolve mp state
 
 neighbourNodes :: ValveState -> Reader ValveSystem (HashSet ValveState)
 neighbourNodes state@MkValveState{..} = do
-  system <- ask
-  let cValve@MkValveData{..} = system HM.! _currentValve
+  cValve@MkValveData{..} <- asks (HM.! _currentValve)
   let incremented = state { _currentTime = _currentTime-1, _visited = HS.insert _currentValve _visited }
   let validChildren = filter (\child -> not (child `HS.member` _visited)) _children
   let travelValves = map (\child -> incremented { _currentValve = child }) validChildren
@@ -98,10 +107,29 @@ neighbourNodes state@MkValveState{..} = do
                                _visited = HS.singleton _currentValve --Clear this down because we can visit old nodes again
                                } | shouldOpenValve]
   allValvesOn <- null <$> remainingUsefulValves state
-  let neighbours = if allValvesOn
+  pure $ if allValvesOn
         then HS.singleton incremented --there is no point going anywhere
         else HS.fromList $! travelValves ++ openCurrentValve
-  hsFilterM (canSurpassTotalFlow 1700) neighbours
+
+neighbourNodesWithEstimate :: Integer -> ValveState -> Reader ValveSystem (HashSet ValveState)
+neighbourNodesWithEstimate estimate state = do
+  allNeighbours <- neighbourNodes state
+  hsFilterM (canSurpassTotalFlow estimate) allNeighbours
+
+--If you can turn on the current valve then do. Otherwise, if there is a valve with pressure next ot you then just prefer that
+naiveNeigbourNodes :: ValveState -> Reader ValveSystem (HashSet ValveState)
+naiveNeigbourNodes state = do
+  allNeighbours <- neighbourNodes state
+  let sameNode = find (\s -> s ^. currentValve == state ^. currentValve) allNeighbours --If it's the same node then we're turning a valve on
+  withPressures <- traverse (\state' -> do
+                                valveData <- asks (HM.! (state' ^. currentValve))
+                                pure (valveData ^. flowRate, state')
+                            ) $ toList allNeighbours
+  let maybeBest = maximumByMay (compare `on` fst) withPressures
+  case maybeBest of
+        Nothing -> pure $ maybe allNeighbours HS.singleton sameNode
+        Just (bestFlow, state') -> let df = if bestFlow == 0 then allNeighbours else HS.singleton state'
+                                    in pure $ maybe df HS.singleton sameNode
 
 canSurpassTotalFlow :: Integer -> ValveState -> Reader ValveSystem Bool
 canSurpassTotalFlow max state@MkValveState{..} = do
@@ -146,7 +174,9 @@ doSolve system state = runReader (runMaybeT (solve state)) system
 
 solve :: ValveState -> MaybeT (Reader ValveSystem) Integer
 solve state = do
-  path <- MaybeT $ aStarM neighbourNodes cost heuristic finished (pure state)
-  let finalState = last path
-  pure $ totalPressureReleased (_valvesOn finalState)
+  naivePath <- MaybeT $ aStarM naiveNeigbourNodes cost heuristic finished (pure state)
+  let estimate = totalPressureReleased $ last naivePath ^. valvesOn
+  let dbg :: String = printf "The best value must be higher than: %d" estimate
+  path <- trace dbg $ MaybeT $ aStarM (neighbourNodesWithEstimate estimate) cost heuristic finished (pure state)
+  pure $ totalPressureReleased $ last path ^. valvesOn
 
